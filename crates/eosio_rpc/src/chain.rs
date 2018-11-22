b9bf6ec5505e96454cde13bec7bdd56a5f9ec7ad
Liam Curry <liam@sagan.software>
2018-11-22 10:56:39 -0500

    added chain info type

--- crates/eosio_rpc/src/chain.rs
+++ crates/eosio_rpc/src/chain.rs
@@ -1,6 +1,30 @@
+use eosio::AccountName;
+
+pub type ChainId = String;
+
+pub type BlockId = String;
+
+pub type BlockNum = u32;
+
+pub type ServerVersion = String;
+
+pub type BlockTimestamp = String;
+
+#[derive(::serde::Deserialize, ::serde::Serialize, Debug)]
 pub struct Info {
+    pub server_version: ServerVersion,
+    pub server_version_string: String,
+    pub chain_id: ChainId,
+    pub head_block_num: BlockNum,
+    pub head_block_id: BlockId,
+    pub head_block_time: BlockTimestamp,
+    pub head_block_producer: AccountName,
+    pub last_irreversible_block_num: BlockNum,
+    pub last_irreversible_block_id: BlockId,
     pub virtual_block_cpu_limit: u32,
     pub virtual_block_net_limit: u32,
+    pub block_cpu_limit: u32,
+    pub block_net_limit: u32,
 }
 
 pub fn get_info(node: &str) {}
@@ -16,6 +40,12 @@ pub fn get_abi() {}
 pub fn get_code() {}
 
 pub fn get_raw_code_and_abi() {}
+
+#[derive(::serde::Serialize, ::serde::Deserialize, Debug, Clone, Default)]
+pub struct TableRows<Row> {
+    pub rows: Vec<Row>,
+    pub more: bool,
+}
 
 pub fn get_table_rows() {}
 
--- crates/eosio_rpc/src/lib.rs
+++ crates/eosio_rpc/src/lib.rs
@@ -4,3 +4,8 @@ pub mod chain;
 pub mod history;
 pub mod net;
 pub mod producer;
+
+pub use self::chain::*;
+pub use self::history::*;
+pub use self::net::*;
+pub use self::producer::*;
