6ef25600d9da8589fbaf9f8099635f98f42b7058
Liam Curry <liam@sagan.software>
2018-11-22 10:55:59 -0500

    serialize/deserialize bools as u8

--- crates/eosio_macros/src/eosio_action.rs
+++ crates/eosio_macros/src/eosio_action.rs
@@ -16,8 +16,23 @@ pub fn expand(_args: TokenStream, input: TokenStream) -> TokenStream {
             FnArg::Captured(input) => {
                 let pat = &input.pat;
                 let ty = &input.ty;
+                let ty_str = quote!(#ty).to_string();
+                let serde_attr = if ty_str == "bool" {
+                    quote!(
+                        #[cfg_attr(
+                            feature = "serde",
+                            serde(
+                                deserialize_with = "::eosio::json::bool_from_u8",
+                                serialize_with = "::eosio::json::bool_to_u8"
+                            )
+                        )]
+                    )
+                } else {
+                    quote!()
+                };
                 struct_fields = quote! {
                     #struct_fields
+                    #serde_attr
                     pub #pat: #ty,
                 };
                 assign_args = quote! {
@@ -36,7 +51,8 @@ pub fn expand(_args: TokenStream, input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
         #[derive(Clone, #eosio::Read, #eosio::Write, #eosio::NumBytes)]
-        struct #struct_ident {
+        #[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+        pub struct #struct_ident {
             #(#struct_fields)*
         }
 
@@ -69,14 +85,44 @@ pub fn expand(_args: TokenStream, input: TokenStream) -> TokenStream {
     let inputs = decl.inputs;
     let vis = input.vis;
     let mut struct_fields = quote!();
+    let mut assign_args = quote!();
+    for input in inputs.iter() {
+        match input {
+            FnArg::Captured(input) => {
+                let pat = &input.pat;
+                let ty = &input.ty;
+                let ty_str = quote!(#ty).to_string();
+                let serde_attr = if ty_str == "bool" {
+                    quote!(
+                        #[cfg_attr(
+                            feature = "serde",
+                            serde(
+                                deserialize_with = "::eosio::json::bool_from_u8",
+                                serialize_with = "::eosio::json::bool_to_u8"
+                            )
+                        )]
+                    )
+                } else {
+                    quote!()
+                };
+                struct_fields = quote! {
+                    #struct_fields
+                    #serde_attr
+                    pub #pat: #ty,
+                };
+            }
+            _ => unimplemented!(),
+        }
+    }
 
     let call_site = ::proc_macro2::Span::call_site();
     let struct_name = titlecase(ident.to_string().as_str());
     let struct_ident = Ident::new(format!("{}Action", struct_name).as_str(), call_site);
 
     let expanded = quote! {
-        #[derive(Clone, #eosio::Read, #eosio::Write, #eosio::NumBytes)]
-        struct #struct_ident {
+        #[derive(Clone, #eosio::Read, #eosio::Write, #eosio::NumBytes, Default)]
+        #[cfg_attr(feature = "serde", derive(::serde::Serialize, ::serde::Deserialize))]
+        pub struct #struct_ident {
             #(#struct_fields)*
         }
 
