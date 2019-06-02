be70c8e322db4ce09ef7c789676d34e2e86b0724
Liam Curry <liam@sagan.software>
2019-06-02 18:15:15 -0400

    :tada: release(eosio_numstr_macros): 0.2.1

---
+++ eosio_numstr_macros/tests/ui/name-bad-char.rs
@@ -0,0 +1,7 @@
+extern crate eosio_numstr_macros;
+
+use eosio_numstr_macros::n;
+
+fn main() {
+    let _ = n!(test1230);
+}
---
+++ eosio_numstr_macros/tests/ui/symbol-bad-char.rs
@@ -0,0 +1,7 @@
+extern crate eosio_numstr_macros;
+
+use eosio_numstr_macros::s;
+
+fn main() {
+    let _ = s!(4, Test);
+}
