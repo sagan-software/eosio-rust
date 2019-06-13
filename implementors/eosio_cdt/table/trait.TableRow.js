0540cda53d166f9674e57645aa7781777727e45a
Liam Curry <liam@sagan.software>
2019-06-13 16:47:31 -0400

    update

--- implementors/eosio_cdt/table/trait.TableRow.js
+++ implementors/eosio_cdt/table/trait.TableRow.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["eosio_token"] = [{text:"impl <a class=\"trait\" href=\"eosio_cdt/table/trait.TableRow.html\" title=\"trait eosio_cdt::table::TableRow\">TableRow</a> for <a class=\"struct\" href=\"eosio_token/struct.Account.html\" title=\"struct eosio_token::Account\">Account</a>",synthetic:false,types:["eosio_token::Account"]},{text:"impl <a class=\"trait\" href=\"eosio_cdt/table/trait.TableRow.html\" title=\"trait eosio_cdt::table::TableRow\">TableRow</a> for <a class=\"struct\" href=\"eosio_token/struct.CurrencyStats.html\" title=\"struct eosio_token::CurrencyStats\">CurrencyStats</a>",synthetic:false,types:["eosio_token::CurrencyStats"]},];
+implementors["eosio_token"] = [{text:"impl TableRow for <a class=\"struct\" href=\"eosio_token/struct.Account.html\" title=\"struct eosio_token::Account\">Account</a>",synthetic:false,types:["eosio_token::Account"]},{text:"impl TableRow for <a class=\"struct\" href=\"eosio_token/struct.CurrencyStats.html\" title=\"struct eosio_token::CurrencyStats\">CurrencyStats</a>",synthetic:false,types:["eosio_token::CurrencyStats"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
