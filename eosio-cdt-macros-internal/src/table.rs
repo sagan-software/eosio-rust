38d68da4765aacd28327d4f9437089f279d9d75e
Liam Curry <liam@sagan.software>
2019-09-17 20:21:53 -0400

    add derive from `eosio_core::Table`

--- eosio-cdt-macros-internal/src/table.rs
+++ eosio-cdt-macros-internal/src/table.rs
@@ -6,12 +6,11 @@ use syn::{parse_macro_input, DeriveInput, LitStr};
 pub fn expand(args: TokenStream, input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let eosio_core = crate::paths::eosio_core();
-    let eosio_cdt = crate::paths::eosio_cdt();
     let name = parse_macro_input!(args as Ident);
     let name =
         LitStr::new(format!("{}", quote!(#name)).as_str(), Span::call_site());
     let expanded = quote! {
-        #[derive(Debug, #eosio_core::Read, #eosio_core::Write, #eosio_core::NumBytes, Clone, PartialEq, PartialOrd)]
+        #[derive(Debug, #eosio_core::Table, #eosio_core::Read, #eosio_core::Write, #eosio_core::NumBytes, Clone, PartialEq, PartialOrd)]
         #[table_name = #name]
         #input
     };
