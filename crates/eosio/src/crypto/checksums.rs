37864d3253955209c4bf811e42de24c1212941b6
Liam Curry <liam@sagan.software>
2020-02-15 11:55:58 -0500

    impl PartialEq/PartialOrd for Checksum types

--- crates/eosio/src/crypto/checksums.rs
+++ crates/eosio/src/crypto/checksums.rs
@@ -1,10 +1,13 @@
 use crate::{NumBytes, Read, Write};
+use core::cmp::PartialEq;
 
 macro_rules! checksum_type {
     ($ident:ident, $bytes:literal) => {
         /// TODO docs
         /// TODO Read, Write, `NumBytes` needs a custom implementation based on `fixed_bytes`
-        #[derive(Read, Write, NumBytes, Clone, Copy)]
+        #[derive(
+            Debug, Read, Write, NumBytes, Clone, Copy, PartialEq, PartialOrd,
+        )]
         #[eosio(crate_path = "crate::bytes")]
         pub struct $ident([u8; $bytes]);
 
@@ -19,6 +22,12 @@ macro_rules! checksum_type {
             #[must_use]
             pub const fn to_bytes(&self) -> [u8; $bytes] {
                 self.0
+            }
+
+            /// TODO docs.
+            #[must_use]
+            pub fn as_slice(&self) -> &[u8] {
+                &self.0
             }
         }
 
@@ -50,9 +59,87 @@ macro_rules! checksum_type {
                 self
             }
         }
+
+        impl PartialEq<[u8]> for $ident {
+            fn eq(&self, other: &[u8]) -> bool {
+                &self.0 == other
+            }
+        }
+
+        impl PartialEq<$ident> for &[u8] {
+            fn eq(&self, other: &$ident) -> bool {
+                self == &other.0
+            }
+        }
+
+        impl PartialEq<[u8; $bytes]> for $ident {
+            fn eq(&self, other: &[u8; $bytes]) -> bool {
+                &self.0 == other
+            }
+        }
+
+        impl PartialEq<$ident> for [u8; $bytes] {
+            fn eq(&self, other: &$ident) -> bool {
+                self == &other.0
+            }
+        }
     };
 }
 
 checksum_type!(Checksum160, 20);
 checksum_type!(Checksum256, 32);
-checksum_type!(Checksum512, 64);
+
+/// TODO docs
+/// TODO Read, Write, `NumBytes` needs a custom implementation based on `fixed_bytes`
+#[derive(Read, Write, NumBytes, Clone, Copy)]
+#[eosio(crate_path = "crate::bytes")]
+pub struct Checksum512([u8; 64]);
+
+impl Checksum512 {
+    /// TODO docs.
+    #[must_use]
+    pub const fn as_bytes(&self) -> &[u8; 64] {
+        &self.0
+    }
+
+    /// TODO docs.
+    #[must_use]
+    pub const fn to_bytes(&self) -> [u8; 64] {
+        self.0
+    }
+
+    /// TODO docs.
+    #[must_use]
+    pub fn as_slice(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl From<[u8; 64]> for Checksum512 {
+    #[inline]
+    #[must_use]
+    fn from(value: [u8; 64]) -> Self {
+        Self(value)
+    }
+}
+
+impl From<Checksum512> for [u8; 64] {
+    #[inline]
+    #[must_use]
+    fn from(value: Checksum512) -> Self {
+        value.0
+    }
+}
+
+impl Default for Checksum512 {
+    fn default() -> Self {
+        Self([0; 64])
+    }
+}
+
+impl AsRef<Checksum512> for Checksum512 {
+    #[inline]
+    fn as_ref(&self) -> &Self {
+        self
+    }
+}
